8e922620a359a641c6df1472d34bd59c6fe3f40a
Benjamin Hindman <bhindman@proton.me>
2026-03-25 11:17:02 -0500

    Refactor cashout notification & MoneyAmounts

---
+++ src/domain/shared/money/BTCAmount.ts
@@ -0,0 +1,32 @@
+import Money, { Round } from "../bigint-money"
+import { MoneyAmount } from "./MoneyAmount"
+import { WalletCurrency } from "../primitives"
+import { BigIntConversionError } from "../errors"
+
+export class BtcAmount extends MoneyAmount {
+  currencyCode = WalletCurrency.Btc as WalletCurrency
+
+  private constructor(amount: Money | bigint | string | number) {
+    super(amount, WalletCurrency.Btc)
+  }
+
+  static sats(c: string): BtcAmount | BigIntConversionError {
+    try {
+      return new BtcAmount(c)
+    } catch (error) {
+      return new BigIntConversionError(error instanceof Error ? error.message : String(error))
+    }
+  }
+
+  asSats(precision: number = 0): string {
+    return this.money.toFixed(precision)
+  }
+
+  getInstance(amount: Money): this {
+    return new BtcAmount(amount) as this
+  }
+
+  i18n(): string {
+    throw new Error("Method not implemented.")
+  }
+}
